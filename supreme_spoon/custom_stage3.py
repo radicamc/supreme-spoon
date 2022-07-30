fix averaging over incorrect axis in wavebin limits

supreme_spoon/custom_stage3.py
@@ -108,7 +108,7 @@ def construct_lightcurves(datafiles, output_dir, out_frames,
     header_dict['Transx'] = extract_params['soss_transform_x']
     header_dict['Transy'] = extract_params['soss_transform_y']
     header_dict['Transth'] = extract_params['soss_transform_t']
-    nint = np.shape(flux_o1_clip)[1]
+    nint = np.shape(flux_o1_clip)[0]
     wl1, wu1 = utils.get_wavebin_limits(wave1d_o1)
     wl2, wu2 = utils.get_wavebin_limits(wave1d_o2)
     wl1 = np.repeat(wl1[np.newaxis, :], nint, axis=0)