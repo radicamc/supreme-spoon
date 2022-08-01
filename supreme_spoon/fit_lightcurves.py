Fix error calculation for light curve fits

supreme_spoon/fit_lightcurves.py
@@ -147,15 +147,14 @@ for order in orders:
         if skip is False:
             transit_model = results.lc.evaluate('SOSS')
             scatter = np.median(results.posteriors['posterior_samples']['sigma_w_SOSS'])
-            rms = np.nanstd(norm_flux[out_trans]) * 1e6
             nfit = len(np.where(dists != 'fixed')[0])
+            out_dev = utils.outlier_resistant_variance(norm_flux[out_trans])
             plotting.do_lightcurve_plot(t=dataset.times_lc['SOSS'],
                                         data=dataset.data_lc['SOSS'],
-                                        error=scatter/1e6, model=transit_model,
-                                        real_errors=err[:, i]/np.median(flux[out_trans, i]),
-                                        scatter=scatter, rms=rms,
-                                        outpdf=outpdf, nfit=nfit,
-                                        title='bin {0} | {1:.3f}µm'.format(i, wave[0, i]))
+                                        model=transit_model, scatter=scatter,
+                                        out_dev=out_dev, outpdf=outpdf,
+                                        title='bin {0} | {1:.3f}µm'.format(i, wave[0, i]),
+                                        nfit=nfit)
 
             fit_params, posterior_names = [], []
             for param, dist in zip(params, dists):

supreme_spoon/plotting.py
@@ -74,8 +74,8 @@ def plot_2dlightcurves(wave1, flux1, wave2=None, flux2=None, outpdf=None,
         plt.show()
 
 
-def do_lightcurve_plot(t, data, error, model, real_errors, scatter, rms,
-                       outpdf=None, title=None, nfit=8):
+def do_lightcurve_plot(t, data, model, scatter, out_dev, outpdf=None,
+                       title=None, nfit=8):
     def gaus(x, m, s):
         return np.exp(-0.5 * (x - m) ** 2 / s ** 2) / np.sqrt(
             2 * np.pi * s ** 2)
@@ -88,15 +88,15 @@ def do_lightcurve_plot(t, data, error, model, real_errors, scatter, rms,
 
     # Photometry
     ax1 = plt.subplot(gs[0])
-    ax1.errorbar(t, data, yerr=error, fmt='o', capsize=0,
+    ax1.errorbar(t, data, yerr=scatter * 1e-6, fmt='o', capsize=0,
                  color='royalblue', ms=5, alpha=1)
     ax1.plot(t, model, color='black', zorder=100)
     ax1.set_ylabel('Relative Flux', fontsize=14)
     ax1.set_xlim(np.min(t), np.max(t))
     ax1.xaxis.set_major_formatter(plt.NullFormatter())
-    chi2_v = chi2(data * 1e6, model * 1e6, real_errors * 1e6) / (len(t) - nfit)
-    err_mult = scatter / np.nanmean(real_errors*1e6)
-    ax1.text(t[2], np.min(model), r'$\chi_\nu^2 = {:.2f}$''\n'r'$\sigma={:.2f}$ppm''\n'r'$e={:.2f}$'.format(chi2_v, rms, err_mult),
+    chi2_v = chi2(data * 1e6, model * 1e6, out_dev * 1e6) / (len(t) - nfit)
+    err_mult = scatter / (out_dev * 1e6)
+    ax1.text(t[2], np.min(model), r'$\chi_\nu^2 = {:.2f}$''\n'r'$\sigma={:.2f}$ppm''\n'r'$e={:.2f}$'.format(chi2_v, out_dev*1e6, err_mult),
              fontsize=14)
 
     if title is not None: