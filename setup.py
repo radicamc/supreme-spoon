51aa529de83ba922a295580f6883817d3713894a
radicamc <radica@astro.umontreal.ca>
2022-08-29 09:08:41 -0400

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='supreme_spoon',
-      version='0.0.1',
+      version='0.1.0',
       license='MIT',
       author='Michael Radica',
       author_email='michael.radica@umontreal.ca',
